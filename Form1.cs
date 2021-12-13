0304d8094d8c833fa6926639529b89340848b468
ATSUMI KANETA <42095292+acchi17@users.noreply.github.com>
2021-12-14 08:05:58 +0900

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace SampleWithOpenCvSharp
+{
+    public partial class Form1 : Form
+    {
+        private Image oriImg;
+        private Image edgeImg;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonReadImage_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Image files(*.bmp;*.jpg;*.png)|*.bmp;*.jpg;*.png|All files(*.*)|*.*";
+            ofd.Title = "Read image";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    oriImg = Image.FromFile(ofd.FileName);
+                    pictureBoxMain.Image = oriImg;
+                }
+                catch
+                {
+                    pictureBoxMain.Image = null;
+                }
+            }
+        }
+    }
+}
---
+++ MeshInClosed.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Drawing;
+using OpenCvSharp;
+
+namespace SampleWithOpenCvSharp
+{
+    class MeshInClosed
+    {
+        private Mat src;
+        public MeshInClosed()
+        {
+            
+        }
+
+        public bool loadImage(string imgFilePath)
+        {
+            bool result = false;
+
+            try
+            {
+                using (src = Cv2.ImRead(imgFilePath, ImreadModes.Color)) { }
+                result = true;
+            }
+            catch
+            {
+
+            }
+
+            return result;
+        }
+
+        private Mat toGrayScale()
+        {
+            Mat dst = null;
+            if (src != null)
+            {
+                dst = src.CvtColor(ColorConversionCodes.BGR2GRAY);
+            }
+            return dst;
+        }
+    }
+}
